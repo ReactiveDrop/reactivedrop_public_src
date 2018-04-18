d1332a8b84aac8f958c08d4912128dd807d671f0
geotavros <geotavros@gmail.com>
2018-04-18 12:49:37 +0300

    Add utility scripts for building and log
    build_rd_sln.bat double clicking this file will build reactivedrop
    solution in Release configuration without starting Visual Studio up.
    
    build_rd_sln.sh is meant to be run from git bash to build reactivedrop
    solution in Release configuration without starting Visual Studio up.
    Project's root dir needs to be the current working directory for script
    to work correctly.
    
    log.sh is meant to generate log.log file in a format suitable for
    creating the changelog for announcements. Project's root dir needs
    to be the current working directory for script to work correctly.

---
+++ utils/build_rd_sln.sh
@@ -0,0 +1,1 @@
+'/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe' src\\reactivedrop_vs13.sln /target:Build /property:Configuration=Release
---
+++ utils/log.sh
@@ -0,0 +1,1 @@
+git log --pretty=format:"%s%n%b" --reverse > log.log
