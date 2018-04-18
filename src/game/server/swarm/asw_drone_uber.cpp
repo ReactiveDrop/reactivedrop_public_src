67a28f6e36f08b89d3de5ab5488c4c92c3e53386
Raymond Nondorf <raymond.nondorf@gmail.com>
2018-04-18 20:59:23 +0000

    Fix alien_selection SizeScale for asw_drone_uber

--- src/game/server/swarm/asw_drone_uber.cpp
+++ src/game/server/swarm/asw_drone_uber.cpp
@@ -11,6 +11,7 @@
 ConVar asw_drone_uber_health("asw_drone_uber_health", "500", FCVAR_CHEAT, "How much health the uber Swarm drones have");
 ConVar asw_uber_speed_scale("asw_uber_speed_scale", "0.5f", FCVAR_CHEAT, "Speed scale of uber drone compared to normal");
 ConVar asw_uber_auto_speed_scale("asw_uber_auto_speed_scale", "0.3f", FCVAR_CHEAT, "Speed scale of uber drones when attacking");
+ConVar rd_drone_uber_model_scale("rd_drone_uber_model_scale", "1.3", FCVAR_CHEAT, "Scales uber drone model size" );
 extern ConVar asw_alien_hurt_speed;
 extern ConVar asw_alien_stunned_speed;
 extern ConVar rd_deagle_bigalien_dmg_scale;
@@ -19,7 +20,7 @@ extern ConVar rd_deagle_bigalien_dmg_scale;
 
 CASW_Drone_Uber::CASW_Drone_Uber()	
 {
-	
+	m_fSizeScale = rd_drone_uber_model_scale.GetFloat();
 }
 
 CASW_Drone_Uber::~CASW_Drone_Uber()
@@ -33,13 +34,12 @@ BEGIN_DATADESC( CASW_Drone_Uber )
 
 END_DATADESC()
 
-ConVar rd_drone_uber_model_scale("rd_drone_uber_model_scale", "1.3", FCVAR_CHEAT, "Scales uber drone model size" );
 void CASW_Drone_Uber::Spawn( void )
 {	
 	BaseClass::Spawn();
 
-	SetModel( SWARM_NEW_DRONE_MODEL );	
-	SetModelScale(rd_drone_uber_model_scale.GetFloat());
+	SetModel( SWARM_NEW_DRONE_MODEL );
+	SetModelScale( m_fSizeScale );
 	Precache();	
 
 	SetHullType(HULL_LARGE);
