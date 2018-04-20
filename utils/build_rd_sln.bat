6691bf205a1de5d22f21378ecadace640acd0f14
geotavros <geotavros@gmail.com>
2018-04-20 16:55:44 +0300

    Fix build_rd_sln.bat not properly executing

--- utils/build_rd_sln.bat
+++ utils/build_rd_sln.bat
@@ -1,3 +1,3 @@
 cd %~dp0
-"C:Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" ..\src\reactivedrop_vs13.sln /target:Build /property:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" ..\src\reactivedrop_vs13.sln /target:Build /property:Configuration=Release
 pause
