778cc0792b52a865f85712f2ed05541425f0e589
element109 <element109@outlook.com>
2018-04-20 20:33:59 -0400

    Add rd_controller_radius_adjust client convar
    Convar to adjust the cursor radius when using a controller

--- src/game/client/swarm/asw_input.cpp
+++ src/game/client/swarm/asw_input.cpp
@@ -1256,6 +1256,7 @@ void CASWInput::GetSimulatedFullscreenMousePos( int *mx, int *my, int *unclamped
 float MoveToward( float cur, float goal, float lag );
 
 ConVar asw_controller_lag( "asw_controller_lag", "40.0", FCVAR_NONE );
+ConVar rd_controller_radius_adjust( "rd_controller_radius_adjust", "0", FCVAR_NONE, "Number of pixels to add to crosshair distance from marine, when using controller. Can be negative" );
 
 void CASWInput::GetSimulatedFullscreenMousePosFromController( int *mx, int *my, float fControllerPitch, float fControllerYaw, float flForwardFraction )
 {
@@ -1293,9 +1294,10 @@ void CASWInput::GetSimulatedFullscreenMousePosFromController( int *mx, int *my, 
 		//float dist_fraction = length;
 		//if (dist_fraction < 0.9f)
 		//dist_fraction = 0.9f;
+		int radAdjust = rd_controller_radius_adjust.GetInt();
 		int nScreenMin = MIN( ScreenWidth(), ScreenHeight() );
-		*mx = x + ( ( nScreenMin * flForwardFraction ) * cos( DEG2RAD( joy_yaw ) ) );
-		*my = y + ( ( nScreenMin * flForwardFraction ) * sin( DEG2RAD( joy_yaw ) ) );
+		*mx = x + ( ( nScreenMin * flForwardFraction + radAdjust ) * cos( DEG2RAD( joy_yaw ) ) );
+		*my = y + ( ( nScreenMin * flForwardFraction + radAdjust ) * sin( DEG2RAD( joy_yaw ) ) );
 	}
 }
 
