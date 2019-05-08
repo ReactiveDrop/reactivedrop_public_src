bfafe7f2c798a9bedd0fb81f9ac4bc7ebef8c6ea
geotavros <geotavros@gmail.com>
2019-05-08 03:11:45 +0300

    Add rd_grenade_launcher_num_clusters cvar

--- src/game/shared/swarm/asw_weapon_grenade_launcher.cpp
+++ src/game/shared/swarm/asw_weapon_grenade_launcher.cpp
@@ -72,6 +72,7 @@ CASW_Weapon_Grenade_Launcher::~CASW_Weapon_Grenade_Launcher()
 ConVar asw_grenade_launcher_speed( "asw_grenade_launcher_speed", "2.4f", FCVAR_CHEAT, "Scale speed of grenade launcher grenades" );
 ConVar asw_grenade_launcher_gravity( "asw_grenade_launcher_gravity", "2.4f", FCVAR_CHEAT, "Gravity of grenade launcher grenades" );
 ConVar rd_grenade_launcher_explode_on_contact( "rd_grenade_launcher_explode_on_contact", "1", FCVAR_CHEAT, "If set to 0 grenade will not explode on contact with rigid world" );
+ConVar rd_grenade_launcher_num_clusters( "rd_grenade_launcher_num_clusters", "0", FCVAR_CHEAT, "Number of clusters to spawn on grenade explosion", true, 0, true, 15 );
 #endif
 
 void CASW_Weapon_Grenade_Launcher::PrimaryAttack( void )
@@ -100,7 +101,7 @@ void CASW_Weapon_Grenade_Launcher::PrimaryAttack( void )
 	float fGrenadeDamage = GetWeaponDamage();
 	
 	const float &fGrenadeRadius = MarineSkills()->GetSkillBasedValueByMarine(pMarine, ASW_MARINE_SKILL_GRENADES, ASW_MARINE_SUBSKILL_GRENADE_RADIUS);
-	int iClusters = 0; //MarineSkills()->GetSkillBasedValueByMarine(pMarine, ASW_MARINE_SKILL_GRENADES, ASW_MARINE_SUBSKILL_GRENADE_CLUSTERS);
+	int iClusters = rd_grenade_launcher_num_clusters.GetInt(); //MarineSkills()->GetSkillBasedValueByMarine(pMarine, ASW_MARINE_SKILL_GRENADES, ASW_MARINE_SUBSKILL_GRENADE_CLUSTERS);
 	if (asw_debug_marine_damage.GetBool())
 	{
 		Msg("Grenade launcher damage = %f radius = %f clusters = %d\n", fGrenadeDamage, fGrenadeRadius, iClusters);
