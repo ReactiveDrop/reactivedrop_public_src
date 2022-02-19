822938564b1016ecc34f378b3db8307f62f8b115
Ben Lubar <ben.lubar@gmail.com>
2022-02-19 15:56:21 -0600

    clean up cvars for grenade launcher

--- src/game/shared/swarm/asw_weapon_grenade_launcher.cpp
+++ src/game/shared/swarm/asw_weapon_grenade_launcher.cpp
@@ -73,9 +73,10 @@ ConVar asw_grenade_launcher_speed( "asw_grenade_launcher_speed", "2.4f", FCVAR_C
 ConVar rd_grenade_launcher_explode_on_contact( "rd_grenade_launcher_explode_on_contact", "1", FCVAR_CHEAT, "If set to 0 grenade will not explode on contact with rigid world" );
 ConVar rd_grenade_launcher_num_clusters( "rd_grenade_launcher_num_clusters", "0", FCVAR_CHEAT, "Number of clusters to spawn on grenade explosion", true, 0, true, 15 );
 ConVar rda_grenade_launcher_grenade_ricochet("rda_grenade_launcher_grenade_ricochet", "0", FCVAR_CHEAT, "If set to 1 GL grenades ricochet after world collision");
-#endif
-ConVar asw_grenade_launcher_gravity( "asw_grenade_launcher_gravity", "2.4f", FCVAR_CHEAT, "Gravity of grenade launcher grenades" );
+#else
 ConVar rd_grenade_launcher_grenade_preview( "rd_grenade_launcher_grenade_preview", "0", FCVAR_CHEAT, "Draw a predictive arc for a grenade launcher" );
+#endif
+ConVar asw_grenade_launcher_gravity( "asw_grenade_launcher_gravity", "2.4f", FCVAR_CHEAT | FCVAR_REPLICATED, "Gravity of grenade launcher grenades" );
 
 void CASW_Weapon_Grenade_Launcher::PrimaryAttack( void )
 {
