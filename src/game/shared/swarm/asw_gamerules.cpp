5e22dcecc1c30ce72ce379d03dc6b4a747ad37b9
Dubstep Amber <p7enta@gmail.com>
2022-03-16 13:54:02 +0000

    asw_gamerules.cpp 已在 Bitbucket 中在线编辑过

--- src/game/shared/swarm/asw_gamerules.cpp
+++ src/game/shared/swarm/asw_gamerules.cpp
@@ -7982,7 +7982,7 @@ void CAlienSwarm::OnPlayerFullyJoined( CASW_Player *pPlayer )
 		ConVarRef cvar( m_SavedConvars_Challenge.String( i ) );
 		const char *pszDesiredValue = STRING( m_SavedConvars_Challenge[i] );
 
-		if ( cvar.IsValid() )
+		if ( cvar.IsValid() && cvar.IsFlagSet( FCVAR_REPLICATED ) )
 		{
 			cvar.SetValue( pszDesiredValue );
 		}
