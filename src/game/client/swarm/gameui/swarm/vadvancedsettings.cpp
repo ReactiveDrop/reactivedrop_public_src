b09c88347b1143c64852974f99a7970ab8a32a9c
Ben Lubar <ben.lubar@gmail.com>
2022-04-30 22:56:15 -0500

    advanced settings menu

---
+++ src/game/client/swarm/gameui/swarm/vadvancedsettings.cpp
@@ -0,0 +1,366 @@
+#include "cbase.h"
+#include "vadvancedsettings.h"
+#include "nb_header_footer.h"
+#include "filesystem.h"
+#include "vgui/ILocalize.h"
+#include "vdropdownmenu.h"
+#include "vhybridbutton.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+using namespace vgui;
+using namespace BaseModUI;
+
+extern ConVar ui_gameui_modal;
+
+AdvancedSettings::AdvancedSettings( Panel *parent, const char *panelName )
+	: BaseClass( parent, panelName )
+{
+	if ( ui_gameui_modal.GetBool() )
+	{
+		GameUI().PreventEngineHideGameUI();
+	}
+	SetDeleteSelfOnClose( true );
+
+	SetProportional( true );
+
+	SetUpperGarnishEnabled( true );
+	SetLowerGarnishEnabled( true );
+
+	m_pHeaderFooter = new CNB_Header_Footer( this, "HeaderFooter" );
+	m_pHeaderFooter->SetTitle( L"" );
+	m_pHeaderFooter->SetHeaderEnabled( false );
+	m_pHeaderFooter->SetFooterEnabled( true );
+	m_pHeaderFooter->SetGradientBarEnabled( true );
+	m_pHeaderFooter->SetGradientBarPos( 80, 340 );
+
+	m_pSectionName = new BaseModHybridButton( this, "SectionName", L"" );
+
+	m_iCurrentSection = 0;
+
+	LoadSettingDefinitions();
+	CreateSettingControls();
+}
+
+AdvancedSettings::~AdvancedSettings()
+{
+	GameUI().AllowEngineHideGameUI();
+}
+
+void AdvancedSettings::PerformLayout()
+{
+	int x = ScreenWidth() / 2 - scheme()->GetProportionalScaledValue( 180 );
+	int y = scheme()->GetProportionalScaledValue( 150 );
+
+	vgui::Panel *pPrevSetting = m_pSectionName;
+
+	FOR_EACH_VEC( m_SectionDefs, i )
+	{
+		Section_t *pSectionDef = m_SectionDefs[i];
+
+		if ( i == m_iCurrentSection )
+		{
+			m_pSectionName->SetText( pSectionDef->m_Name );
+		}
+
+		FOR_EACH_VEC( pSectionDef->m_Settings, j )
+		{
+			Setting_t *pSettingDef = pSectionDef->m_Settings[j];
+			if ( pSettingDef->m_Type == SETTING_INVALID || !pSettingDef->m_ConVar.IsValid() )
+			{
+				continue;
+			}
+
+			Assert( pSettingDef->m_Type == SETTING_SELECT );
+
+			const char *szConVar = pSettingDef->m_ConVar.GetName();
+			const char *pConVarValue = pSettingDef->m_ConVar.GetString();
+			const char *pConVarDefValue = pSettingDef->m_ConVar.GetDefault();
+
+			DropDownMenu *pSetting = assert_cast<DropDownMenu *>( FindChildByName( VarArgs( "Drp%s", szConVar ) ) );
+			Assert( pSetting );
+			FlyoutMenu *pOptions = assert_cast<FlyoutMenu *>( FindChildByName( VarArgs( "Flm%s", szConVar ) ) );
+			Assert( pOptions );
+
+			if ( !pSetting || !pOptions )
+			{
+				continue;
+			}
+
+			bool bHidden = false;
+
+			if ( i != m_iCurrentSection )
+			{
+				bHidden = true;
+			}
+			else
+			{
+				FOR_EACH_VEC( pSettingDef->m_HideUnless, k )
+				{
+					if ( pSettingDef->m_HideUnless[k]->IsValid() && !pSettingDef->m_HideUnless[k]->GetBool() )
+					{
+						bHidden = true;
+						break;
+					}
+				}
+			}
+
+			if ( bHidden )
+			{
+				pSetting->SetVisible( false );
+				continue;
+			}
+
+			pSetting->SetVisible( true );
+			pSetting->SetPos( x, y );
+			y += scheme()->GetProportionalScaledValue( 20 );
+
+			int iDefault = -1;
+
+			FOR_EACH_VEC( pSettingDef->m_Options, k )
+			{
+				if ( !V_strcmp( pSettingDef->m_Options[k].m_Value, pConVarValue ) )
+				{
+					char szName[256];
+					V_UnicodeToUTF8( pSettingDef->m_Options[k].m_Name, szName, sizeof( szName ) );
+					pSetting->GetButton()->SetDropdownSelection( szName );
+					pOptions->SetInitialSelection( VarArgs( "Btn%d", k ) );
+					iDefault = -1;
+					break;
+				}
+
+				if ( !V_strcmp( pSettingDef->m_Options[k].m_Value, pConVarDefValue ) )
+				{
+					iDefault = k;
+				}
+			}
+
+			if ( iDefault != -1 )
+			{
+				pOptions->SetInitialSelection( VarArgs( "Btn%d", iDefault ) );
+			}
+
+			pPrevSetting->SetNavDown( pSetting );
+			pSetting->SetNavUp( pPrevSetting );
+			pPrevSetting = pSetting;
+		}
+	}
+
+	BaseClass::PerformLayout();
+
+	SetBounds( 0, 0, ScreenWidth(), ScreenHeight() );
+}
+
+void AdvancedSettings::LoadSettingDefinitions()
+{
+	KeyValues::AutoDelete pKV( "AdvancedSettings" );
+	if ( !pKV->LoadFromFile( g_pFullFileSystem, "resource/rd_advanced_settings.txt", "GAME" ) )
+	{
+		Warning( "Could not load advanced settings list!\n" );
+		return;
+	}
+
+	if ( pKV->GetFirstValue() )
+	{
+		Warning( "Advanced settings list contains non-SECTION key: %s\n", pKV->GetFirstValue()->GetName() );
+	}
+
+	FOR_EACH_TRUE_SUBKEY( pKV, pKVSection )
+	{
+		if ( V_strcmp( pKVSection->GetName(), "SECTION" ) )
+		{
+			Warning( "Advanced settings list contains non-SECTION key: %s\n", pKVSection->GetName() );
+			continue;
+		}
+
+		Section_t *pSection = new Section_t();
+		pSection->m_Name = g_pVGuiLocalize->FindSafe( pKVSection->GetString( "title" ) );
+		m_SectionDefs.AddToTail( pSection );
+
+		FOR_EACH_TRUE_SUBKEY( pKVSection, pKVSetting )
+		{
+			const char *szCVarName = pKVSetting->GetString( "cvar" );
+			Setting_t *pSetting = new Setting_t( szCVarName );
+			pSection->m_Settings.AddToTail( pSetting );
+
+			const char *szTypeName = pKVSetting->GetName();
+			if ( !V_strcmp( szTypeName, "SELECT" ) )
+			{
+				pSetting->m_Type = SETTING_SELECT;
+			}
+			else if ( !V_strcmp( szTypeName, "TOGGLE" ) )
+			{
+				if ( pKVSetting->FindKey( "options" ) )
+				{
+					Warning( "Advanced settings TOGGLE with options: %s\n", szCVarName );
+				}
+
+				// TODO: should we add checkboxes instead of making this yet another dropdown?
+				pSetting->m_Type = SETTING_SELECT;
+				pKVSetting->AddSubKey( new KeyValues( "options", "0", "#RD_AdvancedSettings_Toggle_Off", "1", "#RD_AdvancedSettings_Toggle_On" ) );
+			}
+			else
+			{
+				Warning( "Advanced setting contains unknown type %s for %s\n", szTypeName, szCVarName );
+				pSetting->m_Type = SETTING_INVALID;
+			}
+
+			pSetting->m_Name = g_pVGuiLocalize->FindSafe( pKVSetting->GetString( "title" ) );
+
+			switch ( pSetting->m_Type )
+			{
+			case SETTING_SELECT:
+				if ( KeyValues *pOptions = pKVSetting->FindKey( "options" ) )
+				{
+					FOR_EACH_VALUE( pOptions, pOption )
+					{
+						int iOption = pSetting->m_Options.AddToTail();
+						V_strncpy( pSetting->m_Options[iOption].m_Value, pOption->GetName(), sizeof( pSetting->m_Options[iOption].m_Value ) );
+						pSetting->m_Options[iOption].m_Name = g_pVGuiLocalize->FindSafe( pOption->GetString() );
+					}
+				}
+				else
+				{
+					Warning( "Advanced settings SELECT with no options: %s\n", szCVarName );
+				}
+				break;
+			default:
+				break;
+			}
+
+			FOR_EACH_VALUE( pKVSetting, pValue )
+			{
+				if ( FStrEq( pValue->GetName(), "hide_unless" ) )
+				{
+					pSetting->m_HideUnless.AddToTail( new ConVarRef( pValue->GetString() ) );
+				}
+			}
+		}
+	}
+}
+
+void AdvancedSettings::CreateSettingControls()
+{
+	KeyValues::AutoDelete kvButtonSettings( "BtnDropButton" );
+	kvButtonSettings->SetString( "wide", "360" );
+	kvButtonSettings->SetString( "wideatopen", "260" );
+	kvButtonSettings->SetString( "tall", "15" );
+	kvButtonSettings->SetString( "autoResize", "1" );
+	kvButtonSettings->SetString( "style", "DropDownButton" );
+	kvButtonSettings->SetString( "ActivationType", "1" );
+
+	KeyValues::AutoDelete kvOptionSettings( "Btn" );
+	kvOptionSettings->SetString( "wide", "150" );
+	kvOptionSettings->SetString( "tall", "20" );
+	kvOptionSettings->SetString( "autoResize", "1" );
+	kvOptionSettings->SetString( "wrap", "1" );
+	kvOptionSettings->SetString( "style", "FlyoutMenuButton" );
+
+	FOR_EACH_VEC( m_SectionDefs, i )
+	{
+		Section_t *pSectionDef = m_SectionDefs[i];
+
+		FOR_EACH_VEC( pSectionDef->m_Settings, j )
+		{
+			Setting_t *pSettingDef = pSectionDef->m_Settings[j];
+			if ( pSettingDef->m_Type == SETTING_INVALID || !pSettingDef->m_ConVar.IsValid() )
+			{
+				continue;
+			}
+
+			Assert( pSettingDef->m_Type == SETTING_SELECT );
+
+			const char *szConVar = pSettingDef->m_ConVar.GetName();
+
+			DropDownMenu *pSetting = new DropDownMenu( this, VarArgs( "Drp%s", szConVar ) );
+			pSetting->SetZPos( 3 );
+			pSetting->SetSize( scheme()->GetProportionalScaledValue( 360 ), scheme()->GetProportionalScaledValue( 15 ) );
+
+			BaseModHybridButton *pButton = new BaseModHybridButton( pSetting, "BtnDropButton", pSettingDef->m_Name );
+			pButton->SetCommand( VarArgs( "Flm%s", szConVar ) );
+			pButton->ApplySettings( kvButtonSettings );
+
+			FlyoutMenu *pOptions = new FlyoutMenu( this, VarArgs( "Flm%s", szConVar ) );
+			pSetting->SetFlyout( VarArgs( "Flm%s", szConVar ) );
+			pOptions->SetZPos( 4 );
+
+			Panel *pBackground = new Panel( pOptions, "PnlBackground" );
+			pBackground->SetZPos( -1 );
+			pBackground->SetSize( scheme()->GetProportionalScaledValue( 156 ), scheme()->GetProportionalScaledValue( 20 * pSettingDef->m_Options.Count() + 5 ) );
+			pBackground->SetPaintBackgroundEnabled( true );
+			pBackground->SetPaintBorderEnabled( true );
+
+			const char *pConVarValue = pSettingDef->m_ConVar.GetString();
+			char szDropdownSelection[256];
+			V_strncpy( szDropdownSelection, pConVarValue, sizeof( szDropdownSelection ) );
+
+			BaseModHybridButton *pFirstButton = NULL, *pLastButton = NULL;
+			FOR_EACH_VEC( pSettingDef->m_Options, k )
+			{
+				BaseModHybridButton *pOption = new BaseModHybridButton( pOptions, VarArgs( "Btn%d", k ), pSettingDef->m_Options[k].m_Name );
+				pOption->ApplySettings( kvOptionSettings );
+				pOption->SetPos( 0, scheme()->GetProportionalScaledValue( 20 * k ) );
+				pOption->SetCommand( VarArgs( "RunCmd%s %s", szConVar, pSettingDef->m_Options[k].m_Value ) );
+				pOption->AddActionSignalTarget( this );
+
+				if ( !V_strcmp( pConVarValue, pSettingDef->m_Options[k].m_Value ) )
+				{
+					V_UnicodeToUTF8( pSettingDef->m_Options[k].m_Name, szDropdownSelection, sizeof( szDropdownSelection ) );
+				}
+
+				if ( !pFirstButton )
+				{
+					pFirstButton = pOption;
+				}
+
+				if ( pLastButton )
+				{
+					pLastButton->SetNavDown( pOption );
+					pOption->SetNavUp( pLastButton );
+					pOption->PinToSibling( VarArgs( "Btn%d", k - 1 ), PIN_TOPLEFT, PIN_BOTTOMLEFT );
+				}
+
+				pLastButton = pOption;
+			}
+
+			pButton->SetDropdownSelection( szDropdownSelection );
+
+			if ( pFirstButton && pLastButton )
+			{
+				pFirstButton->SetNavUp( pLastButton );
+				pLastButton->SetNavDown( pFirstButton );
+			}
+
+			pOptions->SetVisible( false );
+		}
+	}
+}
+
+void AdvancedSettings::OnCommand( const char *command )
+{
+	if ( const char *szConCommand = StringAfterPrefix( command, "RunCmd" ) )
+	{
+		engine->ExecuteClientCmd( szConCommand );
+		engine->ExecuteClientCmd( "host_writeconfig" );
+		InvalidateLayout();
+	}
+	else if ( !V_strcmp( command, "Back" ) )
+	{
+		OnKeyCodePressed( ButtonCodeToJoystickButtonCode( KEY_XBUTTON_B, CBaseModPanel::GetSingleton().GetLastActiveUserId() ) );
+	}
+	else if ( !V_strcmp( command, "NextSection" ) )
+	{
+		m_iCurrentSection++;
+		if ( m_iCurrentSection >= m_SectionDefs.Count() )
+		{
+			m_iCurrentSection = 0;
+		}
+
+		InvalidateLayout();
+	}
+	else
+	{
+		BaseClass::OnCommand( command );
+	}
+}
---
+++ src/game/client/swarm/gameui/swarm/vadvancedsettings.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "basemodui.h"
+#include "VFlyoutMenu.h"
+
+class CNB_Header_Footer;
+
+namespace BaseModUI
+{
+	class BaseModHybridButton;
+
+	class AdvancedSettings : public CBaseModFrame
+	{
+		DECLARE_CLASS_SIMPLE( AdvancedSettings, CBaseModFrame );
+
+		enum SettingType
+		{
+			SETTING_INVALID,
+			SETTING_SELECT,
+		};
+
+		struct SettingOption_t
+		{
+			const wchar_t *m_Name;
+			char m_Value[256];
+		};
+
+		struct Setting_t
+		{
+			Setting_t( const char *szConVar ) : m_ConVar( szConVar )
+			{
+			}
+
+			SettingType m_Type;
+			const wchar_t *m_Name;
+			ConVarRef m_ConVar;
+			CUtlVectorAutoPurge<ConVarRef *> m_HideUnless;
+
+			CUtlVector<SettingOption_t> m_Options;
+		};
+
+		struct Section_t
+		{
+			const wchar_t *m_Name;
+			CUtlVectorAutoPurge<Setting_t *> m_Settings;
+		};
+
+	public:
+		AdvancedSettings( vgui::Panel *parent, const char *panelName );
+		~AdvancedSettings();
+
+		virtual void PerformLayout();
+		virtual void OnCommand( const char *command );
+
+	private:
+		void LoadSettingDefinitions();
+		void CreateSettingControls();
+
+		CNB_Header_Footer *m_pHeaderFooter;
+		CUtlVectorAutoPurge<Section_t *> m_SectionDefs;
+		BaseModHybridButton *m_pSectionName;
+		int m_iCurrentSection;
+	};
+}
