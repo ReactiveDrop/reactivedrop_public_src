2a024a9f928626b2fd3c3d02479a419ba624a95a
Ben Lubar <ben.lubar@gmail.com>
2022-05-21 16:52:03 -0500

    MissionChooser vscript interface

--- src/game/server/swarm/asw_gamerules_vscript.cpp
+++ src/game/server/swarm/asw_gamerules_vscript.cpp
@@ -16,9 +16,28 @@
 #include "asw_marine.h"
 #include "asw_marine_resource.h"
 #include "asw_game_resource.h"
+#include "asw_campaign_save.h"
+#include "rd_challenges_shared.h"
+#include "rd_missions_shared.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
+
+extern ConVar rd_challenge;
+
+static ScriptVariant_t s_newArrayFunc;
+static ScriptVariant_t s_arrayPushFunc;
+static void CreateArray( ScriptVariant_t &array )
+{
+	Assert( array.m_type == FIELD_VOID );
+	ScriptStatus_t status = g_pScriptVM->Call( s_newArrayFunc, NULL, false, &array );
+	Assert( status == SCRIPT_DONE );
+}
+static void ArrayPush( ScriptVariant_t &array, ScriptVariant_t item )
+{
+	ScriptStatus_t status = g_pScriptVM->Call( s_arrayPushFunc, NULL, false, &array, array, item );
+	Assert( status == SCRIPT_DONE );
+}
 
 // BenLubar(key-values-director): this class provides the Director object for vscripts
 class CASW_Director_VScript
@@ -378,6 +397,299 @@ BEGIN_SCRIPTDESC_ROOT_NAMED( CASW_Convars_VScript, "Convars", SCRIPT_SINGLETON "
 	DEFINE_SCRIPTFUNC( ExecuteConCommand, "Executes the convar command." )
 END_SCRIPTDESC();
 
+class CASW_Mission_Chooser_VScript
+{
+	void ChallengeToVScript( ScriptVariant_t &table, const char *szID, KeyValues *pKV )
+	{
+		g_pScriptVM->CreateTable( table );
+
+		g_pScriptVM->SetValue( table, "id", szID );
+		PublishedFileId_t iWorkshopID = ReactiveDropChallenges::WorkshopID( szID );
+		g_pScriptVM->SetValue( table, "workshop", CFmtStr( "%llu", iWorkshopID ) );
+		g_pScriptVM->SetValue( table, "has_script", filesystem->FileExists( CFmtStr( "scripts/vscripts/challenge_%s.nut", szID ), "GAME" ) );
+
+		g_pScriptVM->SetValue( table, "name", pKV->GetString( "name" ) );
+		g_pScriptVM->SetValue( table, "icon", pKV->GetString( "icon" ) );
+		g_pScriptVM->SetValue( table, "description", pKV->GetString( "description" ) );
+		g_pScriptVM->SetValue( table, "author", pKV->GetString( "author" ) );
+
+		ScriptVariant_t convarTable;
+		g_pScriptVM->CreateTable( convarTable );
+
+		if ( KeyValues *pConVars = pKV->FindKey( "convars" ) )
+		{
+			FOR_EACH_VALUE( pConVars, pConVar )
+			{
+				g_pScriptVM->SetValue( convarTable, pConVar->GetName(), pConVar->GetString() );
+			}
+		}
+
+		g_pScriptVM->SetValue( table, "convars", convarTable );
+		g_pScriptVM->ReleaseValue( convarTable );
+	}
+	void CampaignToVScript( ScriptVariant_t &table, const RD_Campaign_t *pCampaign )
+	{
+		if ( !pCampaign )
+		{
+			table = SCRIPT_VARIANT_NULL;
+			return;
+		}
+
+		g_pScriptVM->CreateTable( table );
+
+		g_pScriptVM->SetValue( table, "id", pCampaign->BaseName );
+		g_pScriptVM->SetValue( table, "workshop", CFmtStr( "%llu", pCampaign->WorkshopID ) );
+
+		g_pScriptVM->SetValue( table, "name", STRING( pCampaign->CampaignName ) );
+		g_pScriptVM->SetValue( table, "description", STRING( pCampaign->CampaignDescription ) );
+		g_pScriptVM->SetValue( table, "credits", STRING( pCampaign->CustomCreditsFile ) );
+
+		g_pScriptVM->SetValue( table, "icon", STRING( pCampaign->ChooseCampaignTexture ) );
+		g_pScriptVM->SetValue( table, "material", STRING( pCampaign->CampaignTextureName ) );
+		g_pScriptVM->SetValue( table, "material1", STRING( pCampaign->CampaignTextureLayer[0] ) );
+		g_pScriptVM->SetValue( table, "material2", STRING( pCampaign->CampaignTextureLayer[1] ) );
+		g_pScriptVM->SetValue( table, "material3", STRING( pCampaign->CampaignTextureLayer[2] ) );
+
+		g_pScriptVM->SetValue( table, "x", pCampaign->GalaxyX );
+		g_pScriptVM->SetValue( table, "y", pCampaign->GalaxyY );
+
+		static_assert( ASW_NUM_SEARCH_LIGHTS == 4, "ASW_NUM_SEARCH_LIGHTS changed" );
+		g_pScriptVM->SetValue( table, "search_light_x_1", pCampaign->SearchLightX[0] );
+		g_pScriptVM->SetValue( table, "search_light_y_1", pCampaign->SearchLightY[0] );
+		g_pScriptVM->SetValue( table, "search_light_angle_1", pCampaign->SearchLightAngle[0] );
+		g_pScriptVM->SetValue( table, "search_light_x_2", pCampaign->SearchLightX[1] );
+		g_pScriptVM->SetValue( table, "search_light_y_2", pCampaign->SearchLightY[1] );
+		g_pScriptVM->SetValue( table, "search_light_angle_2", pCampaign->SearchLightAngle[1] );
+		g_pScriptVM->SetValue( table, "search_light_x_3", pCampaign->SearchLightX[2] );
+		g_pScriptVM->SetValue( table, "search_light_y_3", pCampaign->SearchLightY[2] );
+		g_pScriptVM->SetValue( table, "search_light_angle_3", pCampaign->SearchLightAngle[2] );
+		g_pScriptVM->SetValue( table, "search_light_x_4", pCampaign->SearchLightX[3] );
+		g_pScriptVM->SetValue( table, "search_light_y_4", pCampaign->SearchLightY[3] );
+		g_pScriptVM->SetValue( table, "search_light_angle_4", pCampaign->SearchLightAngle[3] );
+
+		ScriptVariant_t missionsArray;
+		CreateArray( missionsArray );
+
+		FOR_EACH_VEC( pCampaign->Missions, i )
+		{
+			const RD_Campaign_Mission_t *pMission = &pCampaign->Missions[i];
+			ScriptVariant_t missionTable;
+			g_pScriptVM->CreateTable( missionTable );
+
+			g_pScriptVM->SetValue( missionTable, "index", pMission->CampaignIndex );
+			g_pScriptVM->SetValue( missionTable, "map", pMission->MapName );
+			g_pScriptVM->SetValue( missionTable, "name", STRING( pMission->MissionName ) );
+			g_pScriptVM->SetValue( missionTable, "location_description", STRING( pMission->LocationDescription ) );
+			g_pScriptVM->SetValue( missionTable, "short_briefing", STRING( pMission->ShortBriefing ) );
+			g_pScriptVM->SetValue( missionTable, "threat_string", STRING( pMission->ThreatString ) );
+			g_pScriptVM->SetValue( missionTable, "x", pMission->LocationX );
+			g_pScriptVM->SetValue( missionTable, "y", pMission->LocationY );
+			g_pScriptVM->SetValue( missionTable, "difficulty_modifier", pMission->DifficultyModifier );
+			g_pScriptVM->SetValue( missionTable, "always_visible", pMission->AlwaysVisible );
+			g_pScriptVM->SetValue( missionTable, "needs_more_than_one_marine", pMission->NeedsMoreThanOneMarine );
+
+			ScriptVariant_t linksArray;
+			CreateArray( linksArray );
+			FOR_EACH_VEC( pMission->Links, j )
+			{
+				ArrayPush( linksArray, pMission->Links[j] );
+			}
+
+			g_pScriptVM->SetValue( missionTable, "links", linksArray );
+			g_pScriptVM->ReleaseValue( linksArray );
+
+			ArrayPush( missionsArray, missionTable );
+			g_pScriptVM->ReleaseValue( missionTable );
+		}
+
+		g_pScriptVM->SetValue( table, "missions", missionsArray );
+		g_pScriptVM->ReleaseValue( missionsArray );
+
+		ScriptVariant_t tagsArray;
+		CreateArray( tagsArray );
+
+		FOR_EACH_VEC( pCampaign->Tags, i )
+		{
+			ArrayPush( tagsArray, STRING( pCampaign->Tags[i] ) );
+		}
+
+		g_pScriptVM->SetValue( table, "tags", tagsArray );
+		g_pScriptVM->ReleaseValue( tagsArray );
+	}
+	void MissionToVScript( ScriptVariant_t &table, const RD_Mission_t *pMission )
+	{
+		if ( !pMission )
+		{
+			table = SCRIPT_VARIANT_NULL;
+			return;
+		}
+
+		g_pScriptVM->CreateTable( table );
+
+		g_pScriptVM->SetValue( table, "id", pMission->BaseName );
+		g_pScriptVM->SetValue( table, "workshop", CFmtStr( "%llu", pMission->WorkshopID ) );
+
+		g_pScriptVM->SetValue( table, "pos_x", pMission->PosX );
+		g_pScriptVM->SetValue( table, "pos_y", pMission->PosY );
+		g_pScriptVM->SetValue( table, "scale", pMission->Scale );
+
+		g_pScriptVM->SetValue( table, "material", STRING( pMission->Material ) );
+		g_pScriptVM->SetValue( table, "briefing_material", STRING( pMission->BriefingMaterial ) );
+
+		ScriptVariant_t verticalSectionsArray;
+		CreateArray( verticalSectionsArray );
+
+		FOR_EACH_VEC( pMission->VerticalSections, i )
+		{
+			ScriptVariant_t sectionTable;
+			g_pScriptVM->CreateTable( sectionTable );
+
+			g_pScriptVM->SetValue( sectionTable, "material", STRING( pMission->VerticalSections[i].Material ) );
+			g_pScriptVM->SetValue( sectionTable, "altitude_min", pMission->VerticalSections[i].AltitudeMin );
+			g_pScriptVM->SetValue( sectionTable, "altitude_max", pMission->VerticalSections[i].AltitudeMax );
+
+			ArrayPush( verticalSectionsArray, sectionTable );
+			g_pScriptVM->ReleaseValue( sectionTable );
+		}
+
+		g_pScriptVM->SetValue( table, "vertical_sections", verticalSectionsArray );
+		g_pScriptVM->ReleaseValue( verticalSectionsArray );
+
+		g_pScriptVM->SetValue( table, "name", STRING( pMission->MissionTitle ) );
+		g_pScriptVM->SetValue( table, "description", STRING( pMission->Description ) );
+		g_pScriptVM->SetValue( table, "icon", STRING( pMission->Image ) );
+		g_pScriptVM->SetValue( table, "credits", STRING( pMission->CustomCreditsFile ) );
+
+		g_pScriptVM->SetValue( table, "author", STRING( pMission->Author ) );
+		g_pScriptVM->SetValue( table, "website", STRING( pMission->Website ) );
+		g_pScriptVM->SetValue( table, "version", STRING( pMission->Version ) );
+		// intentionally not including Builtin flag.
+
+		ScriptVariant_t tagsArray;
+		CreateArray( tagsArray );
+
+		FOR_EACH_VEC( pMission->Tags, i )
+		{
+			ArrayPush( tagsArray, STRING( pMission->Tags[i] ) );
+		}
+
+		g_pScriptVM->SetValue( table, "tags", tagsArray );
+		g_pScriptVM->ReleaseValue( tagsArray );
+	}
+public:
+	int CountChallenges()
+	{
+		return ReactiveDropChallenges::Count();
+	}
+	int CountCampaigns()
+	{
+		return ReactiveDropMissions::CountCampaigns();
+	}
+	int CountMissions()
+	{
+		return ReactiveDropMissions::CountMissions();
+	}
+	ScriptVariant_t GetChallenge( int index )
+	{
+		ScriptVariant_t table;
+		KeyValues::AutoDelete pKV( "CHALLENGE" );
+
+		if ( ReactiveDropChallenges::ReadData( pKV, index ) )
+		{
+			ChallengeToVScript( table, ReactiveDropChallenges::Name( index ), pKV );
+		}
+
+		return table;
+	}
+	ScriptVariant_t GetChallengeByName( const char *szName )
+	{
+		ScriptVariant_t table;
+		KeyValues::AutoDelete pKV( "CHALLENGE" );
+
+		if ( ReactiveDropChallenges::ReadData( pKV, szName ) )
+		{
+			ChallengeToVScript( table, szName, pKV );
+		}
+
+		return table;
+	}
+	ScriptVariant_t GetCurrentChallenge()
+	{
+		ScriptVariant_t table;
+		KeyValues::AutoDelete pKV( "CHALLENGE" );
+
+		if ( ReactiveDropChallenges::ReadData( pKV, rd_challenge.GetString() ) )
+		{
+			ChallengeToVScript( table, rd_challenge.GetString(), pKV );
+		}
+
+		return table;
+	}
+	ScriptVariant_t GetCampaign( int index )
+	{
+		ScriptVariant_t table;
+		CampaignToVScript( table, ReactiveDropMissions::GetCampaign( index ) );
+
+		return table;
+	}
+	ScriptVariant_t GetCampaignByName( const char *szName )
+	{
+		ScriptVariant_t table;
+		CampaignToVScript( table, ReactiveDropMissions::GetCampaign( szName ) );
+
+		return table;
+	}
+	ScriptVariant_t GetCurrentCampaign()
+	{
+		ScriptVariant_t table;
+		if ( CAlienSwarm *pGameRules = ASWGameRules() )
+		{
+			if ( CASW_Campaign_Save *pSave = pGameRules->IsCampaignGame() ? pGameRules->GetCampaignSave() : NULL )
+			{
+				CampaignToVScript( table, ReactiveDropMissions::GetCampaign( pSave->GetCampaignName() ) );
+			}
+		}
+
+		return table;
+	}
+	ScriptVariant_t GetMission( int index )
+	{
+		ScriptVariant_t table;
+		MissionToVScript( table, ReactiveDropMissions::GetMission( index ) );
+
+		return table;
+	}
+	ScriptVariant_t GetMissionByName( const char *szName )
+	{
+		ScriptVariant_t table;
+		MissionToVScript( table, ReactiveDropMissions::GetMission( szName ) );
+
+		return table;
+	}
+	ScriptVariant_t GetCurrentMission()
+	{
+		ScriptVariant_t table;
+		MissionToVScript( table, ReactiveDropMissions::GetMission( STRING( gpGlobals->mapname ) ) );
+
+		return table;
+	}
+} g_ASWMissionChooserVScript;
+
+BEGIN_SCRIPTDESC_ROOT_NAMED( CASW_Mission_Chooser_VScript, "MissionChooser", SCRIPT_SINGLETON "Provides an interface for querying campaigns, missions, and challenges on the server." )
+	DEFINE_SCRIPTFUNC( CountChallenges, "Returns the number of installed challenges." )
+	DEFINE_SCRIPTFUNC( CountCampaigns, "Returns the number of installed campaigns." )
+	DEFINE_SCRIPTFUNC( CountMissions, "Returns the number of installed missions." )
+	DEFINE_SCRIPTFUNC( GetChallenge, "Creates a table containing challenge data for a challenge by index." )
+	DEFINE_SCRIPTFUNC( GetChallengeByName, "Creates a table containing challenge data for a challenge by name." )
+	DEFINE_SCRIPTFUNC( GetCurrentChallenge, "Creates a table containing challenge data for a the current challenge." )
+	DEFINE_SCRIPTFUNC( GetCampaign, "Creates a table containing campaign data for a campaign by index." )
+	DEFINE_SCRIPTFUNC( GetCampaignByName, "Creates a table containing campaign data for a campaign by name." )
+	DEFINE_SCRIPTFUNC( GetCurrentCampaign, "Creates a table containing campaign data for the current campaign." )
+	DEFINE_SCRIPTFUNC( GetMission, "Creates a table containing mission overview data for a mission by index." )
+	DEFINE_SCRIPTFUNC( GetMissionByName, "Creates a table containing mission overview data for a mission by name." )
+	DEFINE_SCRIPTFUNC( GetCurrentMission, "Creates a table containing mission overview data for the current mission." )
+END_SCRIPTDESC();
+
 HSCRIPT Grenade_Cluster_Create_VScript( float flDamage, float flRadius, int iClusters, Vector position, Vector angles, Vector velocity, Vector angVelocity )
 {
 	CASW_Grenade_Cluster *pCluster = CASW_Grenade_Cluster::Cluster_Grenade_Create( flDamage, flRadius, iClusters, position, QAngle( VectorExpand( angles ) ), velocity, angVelocity, NULL, NULL );
@@ -544,8 +856,21 @@ HSCRIPT Script_FindNearestNPC( const Vector& vecOrigin, bool bCheckZ, float flRa
 
 void CAlienSwarm::RegisterScriptFunctions()
 {
+	// The VScript API doesn't support arrays, but Squirrel does, and we want to use them. Make some helper functions that we can call later:
+	HSCRIPT arrayHelperScript = g_pScriptVM->CompileScript( "function newArray() { return [] }\nfunction arrayPush(a, v) { a.push(v); return a }\n" );
+	HSCRIPT arrayHelperScope = g_pScriptVM->CreateScope( "arrayhelpers" );
+	ScriptStatus_t status = g_pScriptVM->Run( arrayHelperScript, arrayHelperScope, false );
+	Assert( status == SCRIPT_DONE );
+	g_pScriptVM->ReleaseScript( arrayHelperScript );
+	bool bSuccess = g_pScriptVM->GetValue( arrayHelperScope, "newArray", &s_newArrayFunc );
+	Assert( bSuccess );
+	bSuccess = g_pScriptVM->GetValue( arrayHelperScope, "arrayPush", &s_arrayPushFunc );
+	Assert( bSuccess );
+	g_pScriptVM->ReleaseScope( arrayHelperScope );
+
 	g_pScriptVM->RegisterInstance( &g_ASWDirectorVScript, "Director" );
 	g_pScriptVM->RegisterInstance( &g_ASWConvarsVScript, "Convars" );
+	g_pScriptVM->RegisterInstance( &g_ASWMissionChooserVScript, "MissionChooser" );
 	ScriptRegisterFunctionNamed( g_pScriptVM, Grenade_Cluster_Create_VScript, "CreateGrenadeCluster", "create grenade cluster (damage, radius, count, position, angles, velocity, angularVelocity)" );
 	ScriptRegisterFunctionNamed( g_pScriptVM, Script_DropFreezeGrenade, "DropFreezeGrenade", "Drops a freeze grenade (damage, freezeAmount, radius, position)" );
 	ScriptRegisterFunctionNamed( g_pScriptVM, Script_PlaceHealBeacon, "PlaceHealBeacon", "Places a heal beacon (healAmount, healthPerSecond, infestationCureAmount, duration, radius, position)" );
