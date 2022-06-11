9142a9e137e9b8932e9886d03b4b61d0b0669f31
Dmitriy <geotavros@gmail.com>
2022-06-11 23:40:24 +0300

    Add rd_buzzer_blur ConVar
    It can be used to disable the buzzer blur.
    This ConVar is added as a workaround for #76 and should be removed once
    the issue is fixed.

--- src/game/client/swarm/c_asw_marine.cpp
+++ src/game/client/swarm/c_asw_marine.cpp
@@ -89,6 +89,7 @@ ConVar rd_use_new_prediction_strategy( "rd_use_new_prediction_strategy", "1", FC
 ConVar rd_marine_explodes_into_gibs("rd_marine_explodes_into_gibs", "1", FCVAR_ARCHIVE);
 ConVar rd_marine_gib_lifetime( "rd_marine_gib_lifetime", "36000.0", FCVAR_NONE, "number of seconds before marine gibs fade" );
 ConVar rd_marine_gib_lifetime_dm( "rd_marine_gib_lifetime_dm", "15.0", FCVAR_NONE, "number of seconds before marine gibs fade in deathmatch mode" );
+ConVar rd_buzzer_blur( "rd_buzzer_blur", "1", FCVAR_ARCHIVE, "Set to 0 to disable buzzer blur" );	// TODO: Remove this once the buzzer blur issue is fixed. See #76
 extern ConVar asw_DebugAutoAim;
 extern ConVar rd_revive_duration;
 extern ConVar rd_aim_marines;
@@ -2235,7 +2236,8 @@ int C_ASW_Marine::DrawModel( int flags, const RenderableInstance_t &instance )
 
 void C_ASW_Marine::SetPoisoned(float f)
 {
-	m_fPoison = f;
+	if ( rd_buzzer_blur.GetBool() )
+		m_fPoison = f;
 }
 
 // IK the left hand?
