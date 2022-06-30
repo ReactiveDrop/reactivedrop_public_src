8d5c1458cc5b5b3247f72573bba5ecd19de78769
Ben Lubar <ben.lubar@gmail.com>
2022-06-30 14:35:38 -0500

    remove jpeglib helper

--- src/game/client/swarm/rd_jpeglib_helper.cpp
+++
@@ -1,9 +0,0 @@
-#include "cbase.h"
-
-#if defined(_MSC_VER) && _MSC_VER >= 1930
-extern "C" FILE *__iob_func( void )
-{
-	// this function is called by jpeglib to get stderr
-	return __acrt_iob_func( 2 ) - 2;
-}
-#endif
