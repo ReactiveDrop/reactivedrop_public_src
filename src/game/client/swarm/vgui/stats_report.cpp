dded6d70b810a8604edec7362aa7c429139dc59d
ag3_killstreak <force_army_guy3@yahoo.com>
2022-10-22 13:56:13 -0600

    Forgot to commit this file

--- src/game/client/swarm/vgui/stats_report.cpp
+++ src/game/client/swarm/vgui/stats_report.cpp
@@ -15,46 +15,10 @@
 #include "vgui_avatarimage.h"
 #include "asw_briefing.h"
 #include "rd_missions_shared.h"
+#include "RGB_StatsReportColors_Shared.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include <tier0/memdbgon.h>
-
-Color g_rgbaStatsReportPlayerColors[] =
-{
-	Color( 225, 60, 60, 255 ),
-	Color( 200, 200, 60, 255 ),
-	Color( 60, 225, 60, 255 ),
-	Color( 30, 90, 225, 255 ),
-	Color( 225, 150, 30, 255 ),
-	Color( 225, 60, 150, 255 ),
-	Color( 120, 80, 250, 255 ),
-	// TODO: different colors for players 9-32
-	Color( 150, 200, 225, 255 ),
-	Color( 225, 60, 60, 255 ),
-	Color( 200, 200, 60, 255 ),
-	Color( 60, 225, 60, 255 ),
-	Color( 30, 90, 225, 255 ),
-	Color( 225, 150, 30, 255 ),
-	Color( 225, 60, 150, 255 ),
-	Color( 120, 80, 250, 255 ),
-	Color( 150, 200, 225, 255 ),
-	Color( 225, 60, 60, 255 ),
-	Color( 200, 200, 60, 255 ),
-	Color( 60, 225, 60, 255 ),
-	Color( 30, 90, 225, 255 ),
-	Color( 225, 150, 30, 255 ),
-	Color( 225, 60, 150, 255 ),
-	Color( 120, 80, 250, 255 ),
-	Color( 150, 200, 225, 255 ),
-	Color( 225, 60, 60, 255 ),
-	Color( 200, 200, 60, 255 ),
-	Color( 60, 225, 60, 255 ),
-	Color( 30, 90, 225, 255 ),
-	Color( 225, 150, 30, 255 ),
-	Color( 225, 60, 150, 255 ),
-	Color( 120, 80, 250, 255 ),
-	Color( 150, 200, 225, 255 ),
-};
 
 StatsReport::StatsReport( vgui::Panel *parent, const char *name ) : vgui::EditablePanel( parent, name )
 {
