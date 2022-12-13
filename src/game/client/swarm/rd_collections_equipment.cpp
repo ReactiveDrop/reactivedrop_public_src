9a6dea4d2999b220f05a9c29e87de5f3bb731243
Ben Lubar <ben.lubar@gmail.com>
2022-12-13 14:57:43 -0600

    swarmopedia units advanced setting; change default to meters (don't rename var to reset as beta testers are probably fine with hammer units or can find the setting)

--- src/game/client/swarm/rd_collections_equipment.cpp
+++ src/game/client/swarm/rd_collections_equipment.cpp
@@ -20,7 +20,7 @@
 #include "tier0/memdbgon.h"
 
 
-ConVar rd_swarmopedia_units_preference( "rd_swarmopedia_units_preference", "0", FCVAR_ARCHIVE, "0=hammer, 1=metric, 2=imperial" );
+ConVar rd_swarmopedia_units_preference( "rd_swarmopedia_units_preference", "1", FCVAR_ARCHIVE, "0=hammer, 1=metric, 2=imperial" );
 ConVar rd_swarmopedia_units_per_foot( "rd_swarmopedia_units_per_foot", "16", FCVAR_NONE, "recommended: 12 to 16" );
 ConVar rd_swarmopedia_units_per_meter( "rd_swarmopedia_units_per_meter", "52.49", FCVAR_NONE, "recommended: 39.37 to 52.49" );
 
