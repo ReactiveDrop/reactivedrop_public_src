5ebe135ba29e564d4f4fda0257b19ec2a6dd4752
Ben Lubar <ben.lubar@gmail.com>
2022-12-14 17:19:59 -0600

    deathmatch mode code cleanup

--- src/game/shared/swarm/asw_deathmatch_mode.h
+++ src/game/shared/swarm/asw_deathmatch_mode.h
@@ -11,7 +11,7 @@
 #define TEAM_ALPHA		FIRST_GAME_TEAM
 #define TEAM_BETA		(TEAM_ALPHA + 1)
 
-#define RD_FRAGS_LEFT_SIZE 6// determines the size of array, how many "# frags left" sounds to play 
+#define RD_FRAGS_LEFT_SIZE 6 // determines the size of array, how many "# frags left" sounds to play 
 #define RD_MAX_GUNGAME_WEAPONS 32
 
 #ifdef CLIENT_DLL
@@ -22,15 +22,16 @@ class CASW_Marine;
 class CASW_Player;
 class CASW_Marine_Resource;
 
-enum RdGameModes 
+enum RdGameModes
 {
-	GAMEMODE_DEATHMATCH, 
-	GAMEMODE_INSTAGIB, 
+	GAMEMODE_DEATHMATCH,
+	GAMEMODE_INSTAGIB,
 	GAMEMODE_GUNGAME,
 	GAMEMODE_TEAMDEATHMATCH
 };
 
-class CASW_Deathmatch_Mode : public CBaseEntity {
+class CASW_Deathmatch_Mode : public CBaseEntity
+{
 public:
 	DECLARE_CLASS( CASW_Deathmatch_Mode, CBaseEntity );
 	DECLARE_NETWORKCLASS();
@@ -42,34 +43,34 @@ public:
 	CASW_Deathmatch_Mode();
 	virtual ~CASW_Deathmatch_Mode();
 
-	bool IsDeathmatchEnabled() const	{ return m_iGameMode == GAMEMODE_DEATHMATCH; }
-	bool IsInstagibEnabled() const		{ return m_iGameMode == GAMEMODE_INSTAGIB; }
-	bool IsGunGameEnabled()	const 		{ return m_iGameMode == GAMEMODE_GUNGAME; }
-	bool IsTeamDeathmatchEnabled() const{ return m_iGameMode == GAMEMODE_TEAMDEATHMATCH; }
-	RdGameModes GetGameMode() const		{ return (RdGameModes)m_iGameMode.Get(); }	 
+	bool IsDeathmatchEnabled() const { return m_iGameMode == GAMEMODE_DEATHMATCH; }
+	bool IsInstagibEnabled() const { return m_iGameMode == GAMEMODE_INSTAGIB; }
+	bool IsGunGameEnabled()	const { return m_iGameMode == GAMEMODE_GUNGAME; }
+	bool IsTeamDeathmatchEnabled() const { return m_iGameMode == GAMEMODE_TEAMDEATHMATCH; }
+	RdGameModes GetGameMode() const { return ( RdGameModes )m_iGameMode.Get(); }
 
 #ifdef GAME_DLL
-	int UpdateTransmitState()	// always send to all clients
+	int UpdateTransmitState() override	// always send to all clients
 	{
 		return SetTransmitState( FL_EDICT_ALWAYS );
 	}
 
-	virtual void LevelInitPostEntity();
+	void LevelInitPostEntity();
 
 	void OnMissionStart();
-	void OnMarineKilled(const CTakeDamageInfo &info, CASW_Marine *marine);
+	void OnMarineKilled( const CTakeDamageInfo &info, CASW_Marine *marine );
 	void OnPlayerFullyJoined( CASW_Player *pPlayer );
 	
 	void DeathmatchEnable();
 
 	void InstagibEnable();
-	void InstagibDisable(bool enable_weapon_respawn_timer = true);
+	void InstagibDisable( bool bEnableWeaponRespawnTimer = true );
 
 	void GunGameEnable();
-	void GunGameDisable(bool enable_weapon_respawn_timer = true);
+	void GunGameDisable( bool bEnableWeaponRespawnTimer = true );
 	
 	void TeamDeathmatchEnable();
-	void TeamDeathmatchDisable(bool enable_weapon_respawn_timer = true);
+	void TeamDeathmatchDisable( bool bEnableWeaponRespawnTimer = true );
 
 	void SpawnMarine( CASW_Player *player );
 	void BroadcastLoadoutScreen();
@@ -81,35 +82,27 @@ public:
 
 	void DeathmatchThink();
 
-	
 private:
 	void DisableWeaponRespawnTimer();
 	void EnableWeaponRespawnTimer();
 
 	void RemoveAllWeaponsFromMap();
-	/** Remove their weapons and give new one, heal, move to spawn point
-		if weapon_id == -1 then this is for GunGame mode, the weapon id will be
-		taken from gun_game_weapons_ array
-	*/
-	void PrepareMarinesForGunGameOrInstagib(int weapon_id = -1);
+	// Remove their weapons and give new one, heal, move to spawn point
+	// if weapon_id == -1 then this is for GunGame mode, the weapon id will be
+	// taken from gun_game_weapons_ array
+	void PrepareMarinesForGunGameOrInstagib( int iWeaponID = -1 );
 
 	void ResetScores();
 
 	void ResetFragsLeftSaid()
 	{
-		for (int i = 0; i < RD_FRAGS_LEFT_SIZE; ++i)
-			frags_left_said[i] = false; 
+		for ( int i = 0; i < RD_FRAGS_LEFT_SIZE; ++i )
+			s_bPlayedFragLimitSound[i] = false;
 	}
 
 	int m_iBotDeaths[ASW_MAX_MARINE_RESOURCES];
 	float m_flBotLastFragTime[ASW_MAX_MARINE_RESOURCES];
 	int m_iBotKillingSpree[ASW_MAX_MARINE_RESOURCES];
-
-	// these static variables are used to preserve the value between map changes
-	// they are read in constructor and written in destructor and are equal to
-	// the network variables that are below
-	static RdGameModes game_mode_; 
-
 #endif // #ifdef GAME_DLL
 
 	// shared code
@@ -119,21 +112,16 @@ public:
 	int GetWeaponIndexByFragsCount( int nFrags );
 
 private:
-	// these network variables are shared and replicated to clients 
+	// these network variables are shared and replicated to clients
 	// in client code they are used mostly for UI code
 	// they hold the current state of the game
-	CNetworkVar(int, m_iGameMode);			// stores current game mode
+	CNetworkVar( int, m_iGameMode ); // stores current game mode
 
 	CNetworkVar( int, m_iGunGameWeaponCount );
 	CNetworkArray( int, m_iGunGameWeapons, RD_MAX_GUNGAME_WEAPONS ); // holds indexes to weapons which will be given in Gun Game mode
 
 #ifdef GAME_DLL
-
-private:
-	static CBaseEntity *spawn_point;
-
-	bool frags_left_said[RD_FRAGS_LEFT_SIZE];
-
+	bool s_bPlayedFragLimitSound[RD_FRAGS_LEFT_SIZE];
 #endif // #ifdef GAME_DLL
 };
 
