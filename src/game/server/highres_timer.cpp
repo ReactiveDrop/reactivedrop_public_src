9354ab9eb64d047c6d1d23ad157df6c7791fe46c
Gandalf <mithrand0@users.noreply.github.com>
2023-04-28 10:16:51 +0200

    remove some timer conmsg spam to devmsg

--- src/game/server/highres_timer.cpp
+++ src/game/server/highres_timer.cpp
@@ -10,7 +10,7 @@ void highres_timer_set( float ms )
 {
 	if ( ms != prev ) {
 
-		ConMsg( "[timer] requested: %f ms\n", ms );
+		DevMsg( "[timer] requested: %f ms\n", ms );
 
 		ULONG currentRes;
 		ULONG desiredRes = 15.0f; // 15 ms os default
@@ -19,11 +19,11 @@ void highres_timer_set( float ms )
 		// if zero or negative, restore default os timer resolution
 		if ( ms > 0.00001f ) {
 			desiredRes = round( ms * 1000 * 100 );
-			ConMsg( "[timer] resolution requested: %d\n", desiredRes );
+			DevMsg( "[timer] resolution requested: %d\n", desiredRes );
 		}
 		else
 		{
-			ConMsg( "[timer] restoring default os settings.\n" );
+			DevMsg( "[timer] restoring default os settings.\n" );
 			apply = false; // ignore desiredRes and reset to os default
 		}
 
