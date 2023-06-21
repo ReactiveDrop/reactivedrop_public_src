2d28809d809315c109cf370fecf3a2a17423e30a
Ben Lubar <ben.lubar@gmail.com>
2023-06-21 14:21:07 -0500

    options menu wip

--- src/game/client/swarm/vgui/rd_vgui_settings_options.cpp
+++ src/game/client/swarm/vgui/rd_vgui_settings_options.cpp
@@ -33,83 +33,77 @@ CRD_VGUI_Settings_Options::CRD_VGUI_Settings_Options( vgui::Panel *parent, const
 	[rd_ammo_counter_under_marine_show_max_ammo]
 	*/
 
-	/*
-	Players
-	[asw_player_names]
-	[cl_chatcolor]
-	[rd_chat_colorful_player_names]
-	[rd_deathmatch_team_colors]
-	[rd_draw_avatars_with_frags]
-	*/
+	// Players
+	m_pSettingPlayerNameMode = new CRD_VGUI_Option( this, "SettingPlayerNameMode", "#rd_option_player_name_mode" );
+	m_pSettingPlayerNameMode->AddOption( 0, "#rd_option_player_name_mode_none", "" );
+	m_pSettingPlayerNameMode->AddOption( 1, "#rd_option_player_name_mode_player", "" );
+	m_pSettingPlayerNameMode->AddOption( 2, "#rd_option_player_name_mode_marine", "" );
+	m_pSettingPlayerNameMode->AddOption( 3, "#rd_option_player_name_mode_both", "" );
+	m_pSettingPlayerNameMode->LinkToConVar( "asw_player_names", true );
+	m_pSettingPlayerChatColor = new CRD_VGUI_Option( this, "SettingPlayerChatColor", "#rd_option_player_chat_color", CRD_VGUI_Option::MODE_COLOR );
+	m_pSettingPlayerChatColor->LinkToConVar( "cl_chatcolor", true );
+	m_pSettingPlayerChatNamesUseColors = new CRD_VGUI_Option( this, "SettingPlayerChatNameUsesColors", "#rd_option_player_chat_name_uses_colors", CRD_VGUI_Option::MODE_CHECKBOX );
+	m_pSettingPlayerChatNamesUseColors->LinkToConVar( "rd_chat_colorful_player_names", true );
+	m_pSettingPlayerDeathmatchTeamColorMode = new CRD_VGUI_Option( this, "SettingPlayerDeathmatchTeamColorMode", "#rd_option_player_deathmatch_team_color_mode" );
+	m_pSettingPlayerDeathmatchTeamColorMode->AddOption( 1, "#rd_option_player_deathmatch_team_color_mode_ally_enemy", "" );
+	m_pSettingPlayerDeathmatchTeamColorMode->AddOption( 2, "#rd_option_player_deathmatch_team_color_mode_counter_strike", "" );
+	m_pSettingPlayerDeathmatchTeamColorMode->LinkToConVar( "rd_deathmatch_team_colors", false );
+	m_pSettingPlayerDeathmatchDrawTopScoreboard = new CRD_VGUI_Option( this, "SettingPlayerDeathmatchDrawTopScoreboard", "#rd_option_player_deathmatch_draw_top_scoreboard", CRD_VGUI_Option::MODE_CHECKBOX );
+	m_pSettingPlayerDeathmatchDrawTopScoreboard->LinkToConVar( "rd_draw_avatars_with_frags", true );
 
-	/*
-	Hints
-	[rd_fail_advice]
-	[gameinstructor_enable]
-	[rd_deathmatch_respawn_hints]
-	[rd_swarmopedia_grid]
-	[rd_swarmopedia_units_preference]
-	*/
+	// Hints
+	// TODO: [rd_fail_advice]
+	// TODO: [gameinstructor_enable]
+	// TODO: [rd_deathmatch_respawn_hints]
+	// TODO: [rd_swarmopedia_grid]
+	// TODO: [rd_swarmopedia_units_preference]
 
-	/*
-	Death
-	[asw_marine_death_cam]
-	[asw_marine_death_cam_slowdown]
-	[rd_marine_explodes_into_gibs]
-	*/
+	// Death
+	// TODO: [asw_marine_death_cam]
+	// TODO: [asw_marine_death_cam_slowdown]
+	// TODO: [rd_marine_explodes_into_gibs]
 
-	/*
-	Advanced Controls
-	[rd_wire_tile_alternate]
-	[rd_sniper_scope_weapon_switch]
-	[in_lock_mouse_to_window]
-	[asw_horizontal_autoaim]
-	*/
+	// Advanced Controls
+	// TODO: [rd_wire_tile_alternate]
+	// TODO: [rd_sniper_scope_weapon_switch]
+	// TODO: [in_lock_mouse_to_window]
+	// TODO: [asw_horizontal_autoaim]
 
-	/*
-	Crosshair
-	[asw_marine_labels_cursor_maxdist]
-	[asw_crosshair_progress_size]
-	[asw_crosshair_use_perspective]
-	[asw_laser_sight]
-	*/
+	// Crosshair
+	// TODO: [asw_marine_labels_cursor_maxdist]
+	// TODO: [asw_crosshair_progress_size]
+	// TODO: [asw_crosshair_use_perspective]
+	// TODO: [asw_laser_sight]
 
-	/*
-	Reloading
-	[asw_auto_reload]
-	[asw_fast_reload_under_marine]
-	[rd_fast_reload_under_marine_scale]
-	[rd_fast_reload_under_marine_height_scale]
-	*/
+	// Reloading
+	// TODO: [asw_auto_reload]
+	// TODO: [asw_fast_reload_under_marine]
+	// TODO: [rd_fast_reload_under_marine_scale]
+	// TODO: [rd_fast_reload_under_marine_height_scale]
 
-	/*
-	Damage Numbers
-	[asw_floating_number_type]
-	[asw_floating_number_combine]
-	*/
+	// Damage Numbers
+	// TODO: [asw_floating_number_type]
+	// TODO: [asw_floating_number_combine]
 
-	/*
-	Speed Running
-	[rd_draw_timer]
-	[rd_draw_timer_color]
-	[rda_print_chat_objective_completion_time]
-	[rda_print_console_objective_completion_time]
-	[cl_auto_restart_mission]
-	*/
+	// Speed Running
+	// TODO: [rd_draw_timer]
+	// TODO: [rd_draw_timer_color]
+	// TODO: [rda_print_chat_objective_completion_time]
+	// TODO: [rda_print_console_objective_completion_time]
+	// TODO: [cl_auto_restart_mission]
 
-	/*
-	Leaderboards
-	[rd_leaderboard_enabled_client]
-	[rd_show_leaderboard_loading]
-	[rd_show_leaderboard_debrief]
-	*/
+	// Leaderboards
+	// TODO: [rd_leaderboard_enabled_client]
+	// TODO: [rd_show_leaderboard_loading]
+	// TODO: [rd_show_leaderboard_debrief]
 
-	/*
-	Loading
-	[rd_show_mission_icon_loading]
-	[rd_loading_image_per_map]
-	[rd_loading_status_text_visible]
-	*/
+	// Loading
+	m_pSettingLoadingMissionIcons = new CRD_VGUI_Option( this, "SettingLoadingMissionIcons", "#rd_option_loading_mission_icons", CRD_VGUI_Option::MODE_CHECKBOX );
+	m_pSettingLoadingMissionIcons->LinkToConVar( "rd_show_mission_icon_loading", true );
+	m_pSettingLoadingMissionScreens = new CRD_VGUI_Option( this, "SettingLoadingMissionScreens", "#rd_option_loading_mission_screens", CRD_VGUI_Option::MODE_CHECKBOX );
+	m_pSettingLoadingMissionScreens->LinkToConVar( "rd_loading_image_per_map", true );
+	m_pSettingLoadingStatusText = new CRD_VGUI_Option( this, "SettingLoadingStatusText", "#rd_option_loading_status_text", CRD_VGUI_Option::MODE_CHECKBOX );
+	m_pSettingLoadingStatusText->LinkToConVar( "rd_loading_status_text_visible", true );
 
 	// Accessibility
 	m_pSettingAccessibilityTracerTintSelf = new CRD_VGUI_Option( this, "SettingAccessibilityTracerTintSelf", "#rd_option_accessibility_tracer_tint_self", CRD_VGUI_Option::MODE_COLOR );
