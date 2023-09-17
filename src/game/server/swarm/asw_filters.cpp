8665e2506e5f44982d902c4f3abbbfb25760bad8
Ben Lubar <ben.lubar@gmail.com>
2023-09-17 15:12:14 -0500

    filter for incapacitated marines

--- src/game/server/swarm/asw_filters.cpp
+++ src/game/server/swarm/asw_filters.cpp
@@ -8,17 +8,17 @@
 
 class CASW_Filter_Rolling : public CBaseFilter
 {
-	DECLARE_CLASS(CASW_Filter_Rolling, CBaseFilter);
+	DECLARE_CLASS( CASW_Filter_Rolling, CBaseFilter );
 
 public:
-	bool PassesFilterImpl(CBaseEntity *pCaller, CBaseEntity *pEntity)
+	bool PassesFilterImpl( CBaseEntity *pCaller, CBaseEntity *pEntity ) override
 	{
-		CASW_Marine *pMarine = CASW_Marine::AsMarine(pEntity);
+		CASW_Marine *pMarine = CASW_Marine::AsMarine( pEntity );
 		return pMarine && pMarine->m_iMeleeAttackID.Get() == CASW_Melee_System::s_nRollAttackID;
 	}
 };
 
-LINK_ENTITY_TO_CLASS(asw_filter_rolling, CASW_Filter_Rolling);
+LINK_ENTITY_TO_CLASS( asw_filter_rolling, CASW_Filter_Rolling );
 
 class CASW_Filter_Marine_Class : public CBaseFilter
 {
@@ -29,7 +29,7 @@ public:
 
 	ASW_Marine_Class m_iMarineClass;
 
-	bool PassesFilterImpl( CBaseEntity *pCaller, CBaseEntity *pEntity )
+	bool PassesFilterImpl( CBaseEntity *pCaller, CBaseEntity *pEntity ) override
 	{
 		CASW_Marine *pMarine = CASW_Marine::AsMarine( pEntity );
 		CASW_Marine_Profile *pProfile = pMarine ? pMarine->GetMarineProfile() : NULL;
@@ -42,3 +42,26 @@ LINK_ENTITY_TO_CLASS( asw_filter_marine_class, CASW_Filter_Marine_Class );
 BEGIN_DATADESC( CASW_Filter_Marine_Class )
 	DEFINE_KEYFIELD( m_iMarineClass, FIELD_INTEGER, "MarineClass" )
 END_DATADESC()
+
+
+class CASW_Filter_Incapacitated : public CBaseFilter
+{
+	DECLARE_CLASS( CASW_Filter_Incapacitated, CBaseFilter );
+
+public:
+	bool PassesFilterImpl( CBaseEntity *pCaller, CBaseEntity *pEntity ) override
+	{
+		CASW_Marine *pMarine = CASW_Marine::AsMarine( pEntity );
+		if ( pMarine )
+			return pMarine->m_bKnockedOut;
+
+#ifdef RD_7A_WEAPONS
+		if ( pEntity && pEntity->Classify() == CLASS_ASW_REVIVE_TOOL_MARKER )
+			return true;
+#endif
+
+		return false;
+	}
+};
+
+LINK_ENTITY_TO_CLASS( asw_filter_incapacitated, CASW_Filter_Incapacitated );
