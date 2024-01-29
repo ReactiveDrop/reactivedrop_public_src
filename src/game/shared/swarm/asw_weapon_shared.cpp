b504905b4eda1270b1836ecdd46520f7513b0072
Ben Lubar <ben.lubar@gmail.com>
2024-01-29 00:29:14 -0600

    fix duplicte magazine gibs in singleplayer

--- src/game/shared/swarm/asw_weapon_shared.cpp
+++ src/game/shared/swarm/asw_weapon_shared.cpp
@@ -226,7 +226,7 @@ void CASW_Weapon::ItemBusyFrame( void )
 				//Msg( "S: %f - %f - %f RELOAD SUCCESS! -- Progress = %f\n", gpGlobals->curtime, fFastStart, fFastEnd, flProgress );
 
 #ifdef CLIENT_DLL
-				if ( prediction->IsFirstTimePredicted() )
+				if ( prediction->InPrediction() && prediction->IsFirstTimePredicted() )
 #endif
 				{
 					pMarine->DoAnimationEvent( PLAYERANIMEVENT_DROP_MAGAZINE_GIB );
@@ -1229,7 +1229,7 @@ void CASW_Weapon::FinishReload( void )
 #endif
 
 #ifdef CLIENT_DLL
-		if ( prediction->IsFirstTimePredicted() )
+		if ( prediction->InPrediction() && prediction->IsFirstTimePredicted() )
 #endif
 		{
 			if ( !m_bFastReloadSuccess )
