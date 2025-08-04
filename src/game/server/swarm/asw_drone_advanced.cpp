1147af4a2263a9b4757d70c5928cc67b3685a47d
Dmitriy <geotavros@gmail.com>
2025-08-05 01:14:12 +0300

    Revert asw_drone changes that affect flinching

--- src/game/server/swarm/asw_drone_advanced.cpp
+++ src/game/server/swarm/asw_drone_advanced.cpp
@@ -60,9 +60,6 @@ ConVar asw_drone_health("asw_drone_health", "40", FCVAR_CHEAT, "How much health 
 ConVar asw_drone_yaw_speed("asw_drone_yaw_speed", "32.0", FCVAR_CHEAT, "How fast the swarm drone can turn");
 ConVar asw_drone_yaw_speed_attackprep("asw_drone_yaw_speed_attackprep", "64.0", FCVAR_CHEAT, "How fast the swarm drone can turn while starting his melee attack");
 ConVar asw_drone_yaw_speed_attacking("asw_drone_yaw_speed_attacking", "8.0", FCVAR_CHEAT, "How fast the swarm drone can turn while doing a melee attack");
-ConVar asw_drone_run_speed( "asw_drone_run_speed", "1.25", FCVAR_CHEAT );
-ConVar asw_drone_attack_speed( "asw_drone_attack_speed", "1.25", FCVAR_CHEAT );
-ConVar asw_drone_attack_speed_on_fire( "asw_drone_attack_speed_on_fire", "1.25", FCVAR_CHEAT );
 ConVar asw_drone_acceleration("asw_drone_acceleration", "5", FCVAR_CHEAT, "How fast the swarm drone accelerates, as a multiplier on his ideal speed");
 ConVar asw_drone_smooth_speed("asw_drone_smooth_speed", "200", FCVAR_CHEAT, "How fast the swarm drone smooths his current velocity into the ideal, when using overidden movement");
 ConVar asw_drone_override_move("asw_drone_override_move", "0", FCVAR_CHEAT, "Enable to make Swarm drones use custom override movement to chase their enemy");
@@ -798,8 +795,6 @@ int CASW_Drone_Advanced::MeleeAttack1Conditions( float flDot, float flDist )
 	if ( flPrDot < 0 )	// try generous way
 		return COND_NOT_FACING_ATTACK;
 
-	if ( GetNextAttack() > gpGlobals->curtime )
-		return COND_TOO_FAR_TO_ATTACK;
 
 #else
 
@@ -1308,18 +1303,14 @@ void CASW_Drone_Advanced::RunTask( const Task_t *pTask )
 		}
 	}
 
-	if ( GetActivity() == ACT_RUN )
-	{
-		SetPlaybackRate( asw_drone_run_speed.GetFloat() );
-	}
-	else if ( GetActivity() == ACT_DRONE_RUN_ATTACKING || GetActivity() == ACT_MELEE_ATTACK1 )
-	{
-		SetPlaybackRate( m_bOnFire ? asw_drone_attack_speed_on_fire.GetFloat() : asw_drone_attack_speed.GetFloat() );
-	}
+	//if (!HasCondition(COND_NPC_FREEZE) && !IsCurSchedule(SCHED_NPC_FREEZE))
+	//{
+	if (GetActivity() == ACT_RUN || GetActivity() == ACT_DRONE_RUN_ATTACKING
+		|| GetActivity() == ACT_MELEE_ATTACK1)
+		m_flPlaybackRate = 1.25f;
 	else
-	{
-		SetPlaybackRate( 1.0f );
-	}
+		m_flPlaybackRate = 1.0f;
+	//}
 }
 
 bool CASW_Drone_Advanced::ShouldGib( const CTakeDamageInfo &info )
@@ -2201,10 +2192,9 @@ bool CASW_Drone_Advanced::IsHeavyDamage( const CTakeDamageInfo &info )
 	}
 	m_FlinchActivity = ACT_INVALID;
 
-	CASW_Weapon *pWeapon = dynamic_cast< CASW_Weapon * >( info.GetWeapon() );
-	if ( pWeapon )
-	{
-		return pWeapon->ShouldAlienFlinch( this, info );
+	if ( pMarine && pMarine->GetActiveASWWeapon() )
+	{		
+		return pMarine->GetActiveASWWeapon()->ShouldAlienFlinch( this, info );
 	}
 
 	return false;
