eda1084fe85b004abbdbfba3b2a4d7719383613a
kitsune <sekwensernl@gmail.com>
2026-03-29 10:01:19 +0200

    build: refactor

--- src/materialsystem/stdshaders/buildsdkshaders.bat
+++ src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -1,48 +1,49 @@
 @echo off
 setlocal
 
-rem ======================================
-rem === PATH CONFIGURATIONS
+REM ======================================
+REM ==== PATH CONFIGURATIONS
 
-rem == Setup path to nmake.exe, from vc common tools directory
+REM == Setup path to nmake.exe, from vc common tools directory
 call "G:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
 
 rem == Set the absolute path to your mod's game directory here
 set GAMEDIR="G:\PENDRIVE\Steam\steamapps\common\Alien Swarm Reactive Drop\reactivedrop"
 
-rem == Set the Path to your mods root source code
-rem == "..\.." should already be correct, accepts relative paths only!
+REM == Set the Path to your mods root source code
+REM == "..\.." should already be correct, accepts relative paths only!
 set SOURCEDIR=..\..
 
-rem === PATH CONFIGURATIONS END
-rem ======================================
-rem ======================================
-rem === SHADER BUILD CONFIGURATION
+REM ==== PATH CONFIGURATIONS END
+REM ======================================
 
-rem == Set force=1 to force shader (re)compilation 
+REM ======================================
+REM ==== SHADER BUILD CONFIGURATION
+
+REM == Set force=1 to force shader recompilation 
 REM set FORCE=1
 
-rem == Set dynamic_shaders=1 to build .inc files only
+REM == Set dynamic_shaders=1 to build .inc files only
 REM set dynamic_shaders=1
 
-rem == Number of threads used by ShaderCompile:
-rem ==  N <= 0 or unset = all threads (default)
-rem ==  N > 0           = force specific number of threads
-rem == Set to override the default behavior.
-REM set THREADS=
+REM == Number of threads used by ShaderCompile:
+REM ==  <= 0 or unset = all threads (default)
+REM ==  > 0           = force specific number of threads
+REM ==  Set to override the default behavior.
+REM set THREADS=-1
 
-rem Optimization level used by ShaderCompile:
-rem   0          = no optimization
-rem   1–2        = intermediate optimization
-rem   3 or unset = full optimization (default)
-rem Set to override the default behavior.
+REM ==  Optimization level used by ShaderCompile:
+REM ==  <= 0          = no optimization
+REM ==  1–2           = intermediate optimization
+REM ==  >= 3 or unset = full optimization (default)
+REM ==  Set to override the default behavior.
 REM set OPTIMIZE=3
 
-rem === SHADER BUILD CONFIGURATION END
-rem ======================================
+REM ==== SHADER BUILD CONFIGURATION END
+REM ======================================
 
-rem ======================================
-rem === RUN
+REM ======================================
+REM ==== RUN
 
 set BUILD_SHADER=call buildshaders.bat
 
