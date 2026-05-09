c25ec52c8af431e73c0addcb0148291b5d763bb6
SuperCake <andrey.feist@web.de>
2026-05-10 01:08:44 +0200

    refactor(weapon): replace repeated code with NotifyIfNoneClip1Ammo

--- src/game/shared/swarm/asw_weapon_flamer_shared.cpp
+++ src/game/shared/swarm/asw_weapon_flamer_shared.cpp
@@ -287,25 +287,7 @@ void CASW_Weapon_Flamer::PrimaryAttack( void )
 
 		// decrement ammo
 		m_iClip1 -= 1;
-#ifdef GAME_DLL
-		if ( m_iClip1 <= 0 && pMarine->GetAmmoCount(m_iPrimaryAmmoType) <= 0 )
-		{
-			// check he doesn't have ammo in an ammo bay
-			CASW_Weapon_Ammo_Bag* pAmmoBag = NULL;
-			CASW_Weapon* pWeapon = pMarine->GetASWWeapon(0);
-			if ( pWeapon && pWeapon->Classify() == CLASS_ASW_AMMO_BAG )
-				pAmmoBag = assert_cast<CASW_Weapon_Ammo_Bag*>(pWeapon);
-
-			if (!pAmmoBag)
-			{
-				pWeapon = pMarine->GetASWWeapon(1);
-				if ( pWeapon && pWeapon->Classify() == CLASS_ASW_AMMO_BAG )
-					pAmmoBag = assert_cast<CASW_Weapon_Ammo_Bag*>(pWeapon);
-			}
-			if ( !pAmmoBag || !pAmmoBag->CanGiveAmmoToWeapon(this) )
-				pMarine->OnWeaponOutOfAmmo(true);
-		}
-#endif
+		NotifyIfNoneClip1Ammo( pMarine );
 
 		/*
 		if (!m_iClip1 && pMarine->GetAmmoCount(m_iPrimaryAmmoType) <= 0)
