c14a3ae360b1e6b4d427d9cbe5114e8e659cbca6
Grimowy <87246477+Kitsune44@users.noreply.github.com>
2026-05-09 16:09:03 +0200

    Update src/game/client/swarm/c_asw_concommands.cpp

--- src/game/client/swarm/c_asw_concommands.cpp
+++ src/game/client/swarm/c_asw_concommands.cpp
@@ -862,7 +862,7 @@ void rd_reset_level_and_promotion_f()
 
 	Msg( "All done, your level and promotion have been reset!\n" );
 }
-ConCommand rd_reset_level_and_promotion( "rd_reset_level_and_promotion", rd_reset_level_and_promotion_f, "Resets promotion (rank, level etc.)", FCVAR_NOT_CONNECTED );
+ConCommand rd_reset_level_and_promotion( "rd_reset_level_and_promotion", rd_reset_level_and_promotion_f, "Resets promotion (rank, level etc.)", FCVAR_CHEAT | FCVAR_NOT_CONNECTED );
 
 void asw_show_xp_f()
 {
